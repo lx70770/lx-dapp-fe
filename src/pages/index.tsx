55aa89e5a09950d06ab6cc382f0e95a56f15b33e
lixiang03 <lx70770@gmail.com>
2022-07-31 13:00:07 +0800

    feat: test env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import IpfsPage from './_ipfs'
 import LXToken from './_lx_token'
 import Metamask from './_metamask'
 import Network from './_network'
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const env = process.env.DEVELOPMENT_ENV
+    console.log('env', env)
+  }, [])
+
   return (
     <>
       <Network />
